e105f84313339d137e535fe4db7bd95ed4374daa
gasmyr.mougang <gasmyr.mougang@bao-sarl.cm>
2017-08-18 21:17:47 +0100

    setup database

--- src/main/java/cm/gasmyr/mougang/it/sgs/heroku/HerokuConfig.java
+++ src/main/java/cm/gasmyr/mougang/it/sgs/heroku/HerokuConfig.java
@@ -5,7 +5,9 @@ import java.net.URISyntaxException;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HerokuConfig {
 	@Bean
 	public javax.sql.DataSource dataSource() throws URISyntaxException {
